70ecb3f04de4cbfedde424560ee70ea952a3a62c
yasterjoshua14 <49028646+yasterjoshua14@users.noreply.github.com>
2025-07-25 09:46:39 +0800

    Add files via upload

---
+++ student_grading_system/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use Dado\Gs\Models\StudentModel;
+
+require 'vendor/autoload.php';
+
+$student1 = new StudentModel;
+var_dump($student1);
---
+++ student_grading_system/src/Core/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dado\Gs\Core;
+use mysqli;
+class Database {
+    protected $connection;
+    public function __construct(){
+       $host = 'localhost';
+       $db = 'oop';
+       $user = 'root';
+       $pass = '';
+
+       $this->conn = new \mysqli($host, $user, $pass, $db);
+       if ($this->conn->connect_error) {
+            die("connection failed: ") . $this ($this->conn->connect_error);
+       }
+    
+    }   
+}
